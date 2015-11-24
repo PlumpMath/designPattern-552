338a004cd6e6b86e532a9588dfbfcb3edb142758
quanqiang-li <2476056494@qq.com>
2015-11-24 09:06:32 +0800

    把项目所有文件都提交

--- designPattern/src/create/abstractFactory/no/Window.java
+++ designPattern/src/create/abstractFactory/no/Window.java
@@ -6,4 +6,29 @@ public abstract class Window {
 	private int y;
 	private int width;
 	private int heigth;
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public int getWidth() {
+		return width;
+	}
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	public int getHeigth() {
+		return heigth;
+	}
+	public void setHeigth(int heigth) {
+		this.heigth = heigth;
+	}
+	
 }
